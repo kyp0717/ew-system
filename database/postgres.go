4f7def6ddb92845d257caf949bf5bf99f8ae4cbd
Phage Ky <kyp0717@gmail.com>
2024-10-28 08:38:56 -0400

    add lsp

--- database/postgres.go
+++ database/postgres.go
@@ -1,16 +1,18 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
-  	"fmt"
-	"github.com/kyp0717/ewxback/webapp/model"
+
 	"github.com/kyp0717/ewxback/data/load"
+	"github.com/kyp0717/ewxback/webapp/model"
+
 	// "gorm.io/gorm/logger"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-    "gorm.io/gorm/schema"
+	"gorm.io/gorm/schema"
 )
 
 var PgDBConn *gorm.DB
@@ -22,16 +24,16 @@ func PgConnectDB() {
 	user := os.Getenv("db_user")
 	password := os.Getenv("db_password")
 	dbname := os.Getenv("db_name")
-	dbport:= os.Getenv("db_port")
+	dbport := os.Getenv("db_port")
 
-  fmt.Println("Starting connection with Postgres Db")
-  dsn := user + "://postgres:" + password + "@" +  host + ":" + dbport + "/" + dbname + "?sslmode=disable"
+	fmt.Println("Starting connection with Postgres Db")
+	dsn := user + "://postgres:" + password + "@" + host + ":" + dbport + "/" + dbname + "?sslmode=disable"
 
-  //db, err := gorm.Open(postgres.Open(dsn) , &gorm.Config{})
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-    NamingStrategy: schema.NamingStrategy{
-        SingularTable: true,
-    },})
+	//db, err := gorm.Open(postgres.Open(dsn) , &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		}})
 
 	if err != nil {
 		panic("Database connection failed.")
@@ -41,42 +43,41 @@ func PgConnectDB() {
 
 	PgDBConn = db
 
-// Check if tables exist
-tablesToCheck := []string{"login", "category", "item", "product", "product_group", "product_detail"}
+	// Check if tables exist
+	tablesToCheck := []string{"login", "category", "item", "product", "product_group", "product_detail"}
 
-for _, table := range tablesToCheck {
-	if TableExists(db, table) {
-		fmt.Printf("Table %s exists.\n", table)
-	} else {
-		fmt.Printf("Table %s does not exist.\n", table)
-		
-		// Check which table does not exist and generate table respectively
-		switch table {
-		case "login":
-			db.AutoMigrate( &model.Login{}, )
-			load.Login(db) 
-		case "category":
-			db.AutoMigrate( &model.Category{}, )
-			load.Category(db) 
-		case "item":
-			db.AutoMigrate(	&model.Item{}, )
-			load.Item(db)  
+	for _, table := range tablesToCheck {
+		if TableExists(db, table) {
+			fmt.Printf("Table %s exists.\n", table)
+		} else {
+			fmt.Printf("Table %s does not exist.\n", table)
 
+			// Check which table does not exist and generate table respectively
+			switch table {
+			case "login":
+				db.AutoMigrate(&model.Login{})
+				load.Login(db)
+			case "category":
+				db.AutoMigrate(&model.Category{})
+				load.Category(db)
+			case "item":
+				db.AutoMigrate(&model.Item{})
+				load.Item(db)
 
-		/*	
-		case "product":
-			db.AutoMigrate(	&model.Product{}, )
-			load.Product(db) 
-		case "product_group":
-			db.AutoMigrate(	&model.ProductGroup{},	)
-			load.ProductGroup(db) 
-		case "product_detail":
-			db.AutoMigrate(	&model.ProductDetail{}, )	
-			load.ProductDetail(db) 
-		*/	
+				/*
+					case "product":
+						db.AutoMigrate(	&model.Product{}, )
+						load.Product(db)
+					case "product_group":
+						db.AutoMigrate(	&model.ProductGroup{},	)
+						load.ProductGroup(db)
+					case "product_detail":
+						db.AutoMigrate(	&model.ProductDetail{}, )
+						load.ProductDetail(db)
+				*/
+			}
 		}
 	}
-}
 
 	// Migrate your models
 	err = db.AutoMigrate(
